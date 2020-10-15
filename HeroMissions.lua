0868012df770414b73c4c87efc5b75b5221d6449
Raphaël Bonnet <harcyah@gmail.com>
2020-10-15 21:17:33 +0200

    Bump interface version to 90001

--- HeroMissions.lua
+++ HeroMissions.lua
@@ -1,10 +1,10 @@
 GarrisonLandingPageMinimapButton:HookScript("OnClick", function(self, button, down)
 	if button == "LeftButton" then
-		ShowGarrisonLandingPage(LE_GARRISON_TYPE_8_0);
+		ShowGarrisonLandingPage(Enum.GarrisonType.Type_8_0);
 	elseif button == "RightButton" then
-		ShowGarrisonLandingPage(LE_GARRISON_TYPE_7_0);
+		ShowGarrisonLandingPage(Enum.GarrisonType.Type_7_0);
 	elseif button == "MiddleButton" then
-		ShowGarrisonLandingPage(LE_GARRISON_TYPE_6_0);
+		ShowGarrisonLandingPage(Enum.GarrisonType.Type_6_0);
 	end
 end);
 GarrisonLandingPageMinimapButton:RegisterForClicks("LeftButtonUp", "RightButtonUp", "MiddleButtonUp")
