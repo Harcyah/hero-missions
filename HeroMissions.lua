dacff115882429fa0dd2a1797a017f44c8f4ea86
Raphaël Bonnet <harcyah@gmail.com>
2020-11-20 09:11:41 +0100

    Use single quotes around strings

--- HeroMissions.lua
+++ HeroMissions.lua
@@ -1,10 +1,10 @@
-GarrisonLandingPageMinimapButton:HookScript("OnClick", function(self, button, down)
-	if button == "LeftButton" then
+GarrisonLandingPageMinimapButton:HookScript('OnClick', function(self, button, down)
+	if button == 'LeftButton' then
 		ShowGarrisonLandingPage(Enum.GarrisonType.Type_8_0);
-	elseif button == "RightButton" then
+	elseif button == 'RightButton' then
 		ShowGarrisonLandingPage(Enum.GarrisonType.Type_7_0);
-	elseif button == "MiddleButton" then
+	elseif button == 'MiddleButton' then
 		ShowGarrisonLandingPage(Enum.GarrisonType.Type_6_0);
 	end
 end);
-GarrisonLandingPageMinimapButton:RegisterForClicks("LeftButtonUp", "RightButtonUp", "MiddleButtonUp")
+GarrisonLandingPageMinimapButton:RegisterForClicks('LeftButtonUp', 'RightButtonUp', 'MiddleButtonUp')
