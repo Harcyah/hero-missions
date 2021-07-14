0d1bed32c02a220ca2e260c508cefceb7512a27f
Raphaël Bonnet <harcyah@gmail.com>
2021-07-14 21:14:03 +0200

    Add support for 9.0 missions

--- HeroMissions.lua
+++ HeroMissions.lua
@@ -1,10 +1,19 @@
 GarrisonLandingPageMinimapButton:HookScript('OnClick', function(self, button, down)
 	if button == 'LeftButton' then
-		ShowGarrisonLandingPage(Enum.GarrisonType.Type_8_0);
-	elseif button == 'RightButton' then
-		ShowGarrisonLandingPage(Enum.GarrisonType.Type_7_0);
-	elseif button == 'MiddleButton' then
-		ShowGarrisonLandingPage(Enum.GarrisonType.Type_6_0);
+		if IsShiftKeyDown() then
+			ShowGarrisonLandingPage(Enum.GarrisonType.Type_8_0);
+			return
+		end
+		if IsControlKeyDown()then
+			ShowGarrisonLandingPage(Enum.GarrisonType.Type_7_0);
+			return
+		end
+		if IsAltKeyDown()then
+			ShowGarrisonLandingPage(Enum.GarrisonType.Type_6_0);
+			return
+		end
+		ShowGarrisonLandingPage(Enum.GarrisonType.Type_9_0);
 	end
 end);
-GarrisonLandingPageMinimapButton:RegisterForClicks('LeftButtonUp', 'RightButtonUp', 'MiddleButtonUp')
+
+GarrisonLandingPageMinimapButton:RegisterForClicks('LeftButtonUp');
