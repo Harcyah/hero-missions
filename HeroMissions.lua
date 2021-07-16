ee683e3c86b0d2ec4309a20c7b3bbaf3ab014401
Raphaël Bonnet <harcyah@gmail.com>
2021-07-16 11:37:12 +0200

    Add slash commands

--- HeroMissions.lua
+++ HeroMissions.lua
@@ -17,3 +17,24 @@ GarrisonLandingPageMinimapButton:HookScript('OnClick', function(self, button, do
 end);
 
 GarrisonLandingPageMinimapButton:RegisterForClicks('LeftButtonUp');
+
+SLASH_HERO_MISSIONS_6_01 = '/heromissions60'
+SLASH_HERO_MISSIONS_7_01 = '/heromissions70'
+SLASH_HERO_MISSIONS_8_01 = '/heromissions80'
+SLASH_HERO_MISSIONS_9_01 = '/heromissions90'
+
+SlashCmdList['HERO_MISSIONS_6_0'] = function()
+	ShowGarrisonLandingPage(Enum.GarrisonType.Type_6_0);
+end
+
+SlashCmdList['HERO_MISSIONS_7_0'] = function()
+	ShowGarrisonLandingPage(Enum.GarrisonType.Type_7_0);
+end
+
+SlashCmdList['HERO_MISSIONS_8_0'] = function()
+	ShowGarrisonLandingPage(Enum.GarrisonType.Type_8_0);
+end
+
+SlashCmdList['HERO_MISSIONS_9_0'] = function()
+	ShowGarrisonLandingPage(Enum.GarrisonType.Type_9_0);
+end
